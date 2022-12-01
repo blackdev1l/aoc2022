bb6391ac994cbad61bc5060df7dc7c924ccd70e9
Cristian Achille <blackdevil93@gmail.com>
2022-12-01 11:51:52 +0100

    Day 01 - refactor

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -2,39 +2,15 @@ import java.util.stream.Collectors
 
 class Day01 {
 
+    private fun ints(input: String) = input.split("\n\n")
+        .map { it.split("\n").sumOf { s -> Integer.parseInt(s) } }
+
     fun part1(input: String): Int {
-        var list = (getIntList(input.split(System.lineSeparator())))
-
-
-        list = list.sortedDescending()
-
-        println(list[0])
-
-        return list[0]
+        return  ints(input).max()
     }
 
     fun part2(input: String): Int {
-        var list = (getIntList(input.split(System.lineSeparator())))
-        list = list.sortedDescending()
-
-        return list.take(3).sum()
+        return ints(input).sortedDescending().take(3).sum()
     }
-
-    private fun getIntList(list: List<String>): List<Int> {
-        val res = ArrayList<Int>()
-        var temp: Int = 0
-        for (i in list.indices) {
-            if (list[i].isBlank()) {
-                res.add(temp)
-                temp = 0
-                continue
-            }
-
-            temp += Integer.parseInt(list[i])
-        }
-
-        return res
-    }
-
 
 }
